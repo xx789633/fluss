b109649ce2fcf10ce13eec36ec78900efb6cf357
Wang Cheng <348448708@qq.com>
2025-06-13 14:54:12 +0800

    wip

--- fluss-lake/fluss-lake-lance/src/main/java/com/alibaba/fluss/lake/lance/LanceLakeCatalog.java
+++ fluss-lake/fluss-lake-lance/src/main/java/com/alibaba/fluss/lake/lance/LanceLakeCatalog.java
@@ -3,14 +3,37 @@ package com.alibaba.fluss.lake.lance;
 import com.alibaba.fluss.config.Configuration;
 import com.alibaba.fluss.exception.TableAlreadyExistException;
 import com.alibaba.fluss.lake.lakestorage.LakeCatalog;
+import com.alibaba.fluss.lake.lance.utils.LanceArrowUtils;
 import com.alibaba.fluss.lake.lance.utils.LanceDatasetAdapter;
 import com.alibaba.fluss.metadata.TableDescriptor;
 import com.alibaba.fluss.metadata.TablePath;
 
 import com.lancedb.lance.WriteParams;
+import org.apache.arrow.vector.types.TimeUnit;
+import org.apache.arrow.vector.types.pojo.ArrowType;
+import org.apache.arrow.vector.types.pojo.Field;
+import org.apache.arrow.vector.types.pojo.Schema;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.alibaba.fluss.metadata.TableDescriptor.BUCKET_COLUMN_NAME;
+import static com.alibaba.fluss.metadata.TableDescriptor.OFFSET_COLUMN_NAME;
+import static com.alibaba.fluss.metadata.TableDescriptor.TIMESTAMP_COLUMN_NAME;
 
 /** A Lance implementation of {@link LakeCatalog}. */
 public class LanceLakeCatalog implements LakeCatalog {
+    private static final List<Field> SYSTEM_COLUMNS = new ArrayList<>();
+
+    static {
+        SYSTEM_COLUMNS.add(Field.nullable(BUCKET_COLUMN_NAME, new ArrowType.Int(32, true)));
+        SYSTEM_COLUMNS.add(Field.nullable(OFFSET_COLUMN_NAME, new ArrowType.Int(64, true)));
+        SYSTEM_COLUMNS.add(
+                Field.nullable(
+                        TIMESTAMP_COLUMN_NAME,
+                        new ArrowType.Timestamp(TimeUnit.MICROSECOND, null)));
+    }
+
     private final Configuration options;
 
     public LanceLakeCatalog(Configuration config) {
@@ -24,7 +47,13 @@ public class LanceLakeCatalog implements LakeCatalog {
                 LanceConfig.from(
                         options.toMap(), tablePath.getDatabaseName(), tablePath.getTableName());
         WriteParams params = LanceConfig.genWriteParamsFromConfig(config);
-        LanceDatasetAdapter.createDataset(config.getDatasetUri(), tableDescriptor, params);
+
+        List<Field> fields = new ArrayList<>();
+        fields.addAll(
+                LanceArrowUtils.toArrowSchema(tableDescriptor.getSchema().getRowType())
+                        .getFields());
+        fields.addAll(SYSTEM_COLUMNS);
+        LanceDatasetAdapter.createDataset(config.getDatasetUri(), new Schema(fields), params);
     }
 
     @Override
--- fluss-lake/fluss-lake-lance/src/main/java/com/alibaba/fluss/lake/lance/utils/LanceDatasetAdapter.java
+++ fluss-lake/fluss-lake-lance/src/main/java/com/alibaba/fluss/lake/lance/utils/LanceDatasetAdapter.java
@@ -1,7 +1,6 @@
 package com.alibaba.fluss.lake.lance.utils;
 
 import com.alibaba.fluss.lake.lance.LanceConfig;
-import com.alibaba.fluss.metadata.TableDescriptor;
 
 import com.lancedb.lance.Dataset;
 import com.lancedb.lance.FragmentMetadata;
@@ -10,6 +9,7 @@ import com.lancedb.lance.ReadOptions;
 import com.lancedb.lance.WriteParams;
 import org.apache.arrow.memory.BufferAllocator;
 import org.apache.arrow.memory.RootAllocator;
+import org.apache.arrow.vector.types.pojo.Schema;
 
 import java.util.List;
 
@@ -17,14 +17,8 @@ import java.util.List;
 public class LanceDatasetAdapter {
     private static final BufferAllocator allocator = new RootAllocator(Long.MAX_VALUE);
 
-    public static void createDataset(
-            String datasetUri, TableDescriptor tableDescriptor, WriteParams params) {
-        Dataset.create(
-                        allocator,
-                        datasetUri,
-                        LanceArrowUtils.toArrowSchema(tableDescriptor.getSchema().getRowType()),
-                        params)
-                .close();
+    public static void createDataset(String datasetUri, Schema schema, WriteParams params) {
+        Dataset.create(allocator, datasetUri, schema, params).close();
     }
 
     public static long appendFragments(LanceConfig config, List<FragmentMetadata> fragments) {
